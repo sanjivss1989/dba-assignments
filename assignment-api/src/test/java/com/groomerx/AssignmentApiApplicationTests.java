4c1fe14ee3b956e6a69af13d9d42a95cfc143bab
Sanjib Bhadra <sanjivss1989@gmail.com>
2021-11-03 23:43:20 +0530

    kkkl

---
+++ assignment-api/src/test/java/com/groomerx/AssignmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groomerx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
